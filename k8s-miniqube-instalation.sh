d266d519c08bdb28e559ab1588875612bc443dd4
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-09-11 11:52:54 +0530

    Create k8s-miniqube-instalation.sh

---
+++ k8s-miniqube-instalation.sh
@@ -0,0 +1,16 @@
+{2 CPUs or more
+2GB of free memory
+20GB of free disk space
+Internet connection}
+
+set -x
+
+yum install java -y
+yum install docker -y
+systemctl start docker
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /bin/kubectl
+kubectl version --client
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /bin/minikube 
+minikube start --force
