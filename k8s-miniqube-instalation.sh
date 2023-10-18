fe83f9a73c300afd53f49be15144d7b2c85a6e79
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-10-18 12:14:44 +0530

    Update k8s-miniqube-instalation.sh

--- k8s-miniqube-instalation.sh
+++ k8s-miniqube-instalation.sh
@@ -5,13 +5,15 @@ Internet connection}
 
 set -x
 
-yum install java -y
-yum install docker -y
-systemctl start docker
+sudo yum install java -y
+sudo yum install docker -y
+sudo systemctl start docker
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /bin/kubectl
 kubectl version --client
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /bin/minikube 
 minikube start --force
+minikube delete
+
 # install helm chart
