5f7043a30a7b4fd3dd9ff0e2ab4f8731f2c605b5
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2023-10-27 11:10:56 +0530

    Update tomcat-jenkins_install.sh

--- tomcat-jenkins_install.sh
+++ tomcat-jenkins_install.sh
@@ -10,3 +10,16 @@ chmod 777 startup.sh shutdown.sh catalina.sh
 ./startup.sh
 cd /opt/tomcat/apache-tomcat-10.0.0/webapps/
 wget https://get.jenkins.io/war-stable/2.401.3/jenkins.war
+
+
+//how to resolve 403 error in tomcat
+vi /mnt/apache-tomcat-10.0.0/conf/tomcat-user.xml
+<role rolename="manager-gui"/>
+<role rolename="admin-gui"/>
+<user username="admin" password="admin" roles="admin-gui"/>
+<user username="manager" password="admin" roles="manager-gui"/>
+
+vi /mnt/apache-tomcat-10.0.0/webapps/manager/META-INFO/contex.xml 
+and in same 
+vi /mnt/apache-tomcat-10.0.0/webapps/host-manager/META-INFO/contex.xml
+valve after this   <.. at the end of line  -->!
