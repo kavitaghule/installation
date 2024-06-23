86ace765579cf157aef267caf54fb8041ce01085
kavitaghule <110611046+kavitaghule@users.noreply.github.com>
2024-06-23 21:44:10 +0530

    Update install-jenkins-as-service.sh

--- jenkins/install-jenkins-as-service.sh
+++ jenkins/install-jenkins-as-service.sh
@@ -3,7 +3,9 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 # Add required dependencies for the jenkins package
-sudo yum install fontconfig java-17-openjdk -y
+#sudo yum install java-1.8.0-openjdk-devel -y
+amazon-linux-extras install java-openjdk11 -y
+#sudo yum install fontconfig java-17-openjdk -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
